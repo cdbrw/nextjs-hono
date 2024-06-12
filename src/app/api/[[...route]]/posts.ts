dba053345ef0a504cd39d37ea33b91e915b5610e
cdbrw <thcdbrw@gmail.com>
2024-06-12 18:28:32 -0400

    added small UI

--- src/app/api/[[...route]]/posts.ts
+++ src/app/api/[[...route]]/posts.ts
@@ -1,10 +1,22 @@
 import { Hono } from "hono";
 
+interface Post {
+  id: number;
+  title: string;
+  content: string;
+}
+
+const posts: Post[] = [
+  { id: 1, title: "This is my first post", content: "Just trying out the app" },
+  { id: 2, title: "Subscribe!", content: "More content on the way ;)" },
+  { id: 3, title: "Like the video", content: "Really helps the channel" },
+  { id: 4, title: "Let me know what you'd like to see next", content: ":D" },
+];
+
 const app = new Hono();
 
 app.get("/", async (c) => {
-  // query the database for blog posts
-  return c.json({ posts: [] });
+  return c.json({ posts });
 });
 
 export default app;
